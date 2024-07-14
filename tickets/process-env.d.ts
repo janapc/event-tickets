29e23f01e37e20bef098acf83ab7381020c5a7fe
jana moraes <jana.pc.costa@gmail.com>
2024-07-14 19:52:29 -0300

    refactor: update packages and rename queue variable

--- tickets/process-env.d.ts
+++ tickets/process-env.d.ts
@@ -5,14 +5,13 @@ declare global {
     interface ProcessEnv {
       [key: string]: string | undefined
       MONGO_URI: string
-      QUEUE_NAME: string
+      QUEUE_SEND_TICKET: string
       RABBITMQ_URL: string
       MAIL_FROM: string
       MAIL_HOST: string
       MAIL_PORT: number
       MAIL_AUTH_USER: string
       MAIL_AUTH_PASS: string
-      SERVICE: string
     }
   }
 }
